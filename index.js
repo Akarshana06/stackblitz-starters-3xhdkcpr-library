1c0e4fae1349e4a8bb9f5dcf377988f5bbb1e564
Akarshana06 <akarshana027@gmail.com>
2025-01-31 17:11:35 +0530

    changes are made

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+const library = require('./data.json')
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
